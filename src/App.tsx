aa3ff99d825ce835a085b7eee7696d5a93ed059f
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2024-07-12 16:22:42 -0500

    start

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex justify-center items-center align-middle">
       <h1 className="text-3xl underline">hola mundo</h1>
     </div>
   )
